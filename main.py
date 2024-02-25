679b1c7d4cd4f671a3a92af173a2a2bb4d388625
zimo0110 <imomalievzoirjon@gmail.com>
2024-02-24 17:29:45 -0800

    threshhold lowered, new images added

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from ultralytics import YOLO
-print(f"""number of people in the room: {YOLO('yolov8l.pt').predict("images/img3.jpg", save=True, classes=[0,], conf=0.5, iou=0.2, line_width=2)[0].boxes.shape[0]}""")
+#print(f"""number of people in the room: {YOLO('yolov8l.pt').predict("images/img3.jpg", save=True, classes=[0,], conf=0.5, iou=0.2, line_width=2)[0].boxes.shape[0]}""")
+res = YOLO('yolov8l.pt').predict(f"images/img{10}.jpeg", save=False, classes=[0,], conf=0.35, iou=0.2, line_width=2)[0].boxes.shape[0]
+print(f"img{10}, num ppl: {res}")
